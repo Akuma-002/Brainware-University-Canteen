e778af33160a309a0d94b44f89b01a022b120cd6
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-05-29 13:16:36 +0530

    Navbar Added

---
+++ canteen/src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import bgImage from '/assets/images/image.png';
+export const Home = () => {
+  return (
+    <div className='mainBox' style={{ backgroundImage: `url(${bgImage})` }}>
+        
+    </div>
+  )
+}
